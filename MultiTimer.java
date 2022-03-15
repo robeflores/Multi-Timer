e3598cae00f062c424eaa6ed30193c7c281e69e7
Robert Flores <robert.flores2728@gmail.com>
2022-03-15 11:28:51 -0500

    timer countdown implemented

--- MultiTimer.java
+++ MultiTimer.java
@@ -10,22 +10,52 @@ import java.beans.PropertyChangeListener;
 
 import javax.swing.SwingWorker; 
 class MultiTimer implements ActionListener, PropertyChangeListener{
+    private enum Timer {
+        POMODORO, GENERAL
+    }
 
-    class ToggleTimerTask extends SwingWorker<Void, Void>{
+    int pomTime = 1459;
+    int genTime = 2000;
+
+    class TimerTask extends SwingWorker<Void, Void>{
+        Timer t;
+        int timeLeftInSec;
+        TimerTask(Timer t, int timeLeftInSec){
+            this.t = t;
+            this.timeLeftInSec = timeLeftInSec;
+        }
+
         @Override
         public Void doInBackground() {
-            firePropertyChange("label", "Pomodoro: 25:00", "Pomodoro: 10");
+            int oldTime = timeLeftInSec;
+            while(timeLeftInSec > 0){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignore) {}
+                timeLeftInSec--;
+                switch(t){
+                    case POMODORO:
+                        firePropertyChange("pomTime", oldTime, timeLeftInSec);
+                        break;
+                    default:
+                        System.out.println("ERROR.");
+                }
+                oldTime = timeLeftInSec;
+            }
             return null;
         }
+
         @Override
-        public void done() {}
+        public void done() {
+            System.out.println("Timer finished!");
+        }
     }
 
     //pomodoro
     private JLabel pomLabel;
     private JButton pomStartBtn;
     private JButton pomResetBtn;
-    private ToggleTimerTask pomTask;
+    private TimerTask pomTask;
 
     //general
     private JLabel genLabel;
@@ -143,16 +173,19 @@ class MultiTimer implements ActionListener, PropertyChangeListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == pomStartBtn){
-            System.out.println("Pom start button pressed!");
-            pomTask = new ToggleTimerTask();
+            pomTask = new TimerTask(Timer.POMODORO, pomTime);
             pomTask.addPropertyChangeListener(this);
             pomTask.execute();
         }
     }
     
+    @Override
     public void propertyChange(PropertyChangeEvent e) {
-        if (e.getPropertyName() == "label") {
-            System.out.println((String)e.getNewValue());
+        if (e.getPropertyName() == "pomTime") {
+            pomLabel.setText("Pomodoro: " + (int)e.getNewValue()/60 + ":");
+            if((int)e.getNewValue()%60 < 10)
+                pomLabel.setText(pomLabel.getText() + "0");
+            pomLabel.setText(pomLabel.getText() + (int)e.getNewValue()%60);
         } 
     }
 
